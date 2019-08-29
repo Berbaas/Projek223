af3c364e82d7d23762dc3788f1d4dde262a157d2
Berbaas <bdewet867@gmail.com>
2019-08-29 10:31:08 +0200

    Autosize true gemaak

--- Projek223/Form1.Designer.cs
+++ Projek223/Form1.Designer.cs
@@ -65,6 +65,7 @@
             // 
             // panel1
             // 
+            this.panel1.AutoSize = true;
             this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.panel1.Controls.Add(this.pnlReports);
             this.panel1.Controls.Add(this.pnlPurchases);
@@ -74,7 +75,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(264, 1057);
+            this.panel1.Size = new System.Drawing.Size(267, 1057);
             this.panel1.TabIndex = 0;
             // 
             // pnlReports
@@ -208,16 +209,17 @@
             // 
             // pnlFORMS
             // 
+            this.pnlFORMS.AutoSize = true;
             this.pnlFORMS.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlFORMS.Location = new System.Drawing.Point(3, 41);
+            this.pnlFORMS.Location = new System.Drawing.Point(3, 42);
             this.pnlFORMS.Name = "pnlFORMS";
-            this.pnlFORMS.Size = new System.Drawing.Size(1082, 1016);
+            this.pnlFORMS.Size = new System.Drawing.Size(1082, 1015);
             this.pnlFORMS.TabIndex = 3;
             // 
             // splitter2
             // 
             this.splitter2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.splitter2.Location = new System.Drawing.Point(3, 38);
+            this.splitter2.Location = new System.Drawing.Point(3, 39);
             this.splitter2.Name = "splitter2";
             this.splitter2.Size = new System.Drawing.Size(1082, 3);
             this.splitter2.TabIndex = 2;
@@ -225,20 +227,21 @@
             // 
             // splitter1
             // 
-            this.splitter1.Location = new System.Drawing.Point(0, 38);
+            this.splitter1.Location = new System.Drawing.Point(0, 39);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 1019);
+            this.splitter1.Size = new System.Drawing.Size(3, 1018);
             this.splitter1.TabIndex = 1;
             this.splitter1.TabStop = false;
             // 
             // panel3
             // 
+            this.panel3.AutoSize = true;
             this.panel3.Controls.Add(this.panel5);
             this.panel3.Controls.Add(this.panel4);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(0, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1085, 38);
+            this.panel3.Size = new System.Drawing.Size(1085, 39);
             this.panel3.TabIndex = 0;
             // 
             // panel5
@@ -318,7 +321,9 @@
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
